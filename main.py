7b5578e6928bfc43a141016832eb2ca24923a46f
hansfl <hansfl@kth.se>
2015-07-18 17:43:57 +0200

    Basic Hello World AppServer

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 __author__ = 'hansfl'
 
+from flask import Flask, render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app = Flask(__name__)
+
 if __name__ == "__main__":
-    print "Hello World!"
+    app.run(host="127.0.0.1", port=9999, debug=True)
